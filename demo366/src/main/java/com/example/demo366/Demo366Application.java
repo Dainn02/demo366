cd7de9761c1e33ed288876daf1a229ce993f05e3
ADMIN <phanthanhsinh2002@gmail.com>
2023-05-26 12:51:46 +0700

    nop bai lan 1

---
+++ demo366/src/main/java/com/example/demo366/Demo366Application.java
@@ -0,0 +1,13 @@
+package com.example.demo366;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo366Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo366Application.class, args);
+	}
+
+}
---
+++ demo366/src/main/java/com/example/demo366/repository/IBookRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo366.repository;
+
+import com.example.demo366.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IBookRepository extends JpaRepository<Book,Long> {
+}
