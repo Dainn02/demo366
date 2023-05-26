f61938c3cf5e2288c6fdcba4691a641788bdcf68
ADMIN <phanthanhsinh2002@gmail.com>
2023-05-26 15:47:01 +0700

    nop bai lan 1

--- demo366/src/main/java/com/example/demo366/controller/BookController.java
+++ demo366/src/main/java/com/example/demo366/controller/BookController.java
@@ -6,10 +6,7 @@ import com.example.demo366.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,6 +33,29 @@ public class BookController {
     @PostMapping("/add")
     public String addBook(@ModelAttribute("book") Book book){
         bookService.addBook(book);
+        return "redirect:/books";
+    }
+    @GetMapping("/edit/{id}")
+    public String editBookForm(@PathVariable("id") Long id, Model model) {
+        Book book = bookService.getBookById(id);
+        if (book != null) {
+            model.addAttribute("book", book);
+            model.addAttribute("categories", categoryService.getAllCategories());
+            return "book/edit";
+        } else {
+            return "not-found";
+        }
+    }
+
+    @PostMapping("/edit")
+    public String editBook(@ModelAttribute("book") Book book) {
+        bookService.updateBook(book);
+        return "redirect:/books";
+    }
+    @GetMapping("/delete/{id}")
+
+    public String deleteBook(@PathVariable("id") Long id){
+        bookService.deleteBook(id);
         return "redirect:/books";
     }
 }
